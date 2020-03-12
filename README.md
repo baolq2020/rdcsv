7c75753b9dd9cbf20b7dad98e54dbc41d60b3137
Ly Quoc Bao <bao.ly@dragonflyteam.com>
2020-03-12 16:18:46 +0700

    Init v1

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# rdcsv
+# Read csv file
+
+
+## Setup:
+### `composer require baolq2020/rdcsv:dev-master`
+
+## Example:
+### We have 2 way for using this library:
+#### #You can use function for convert csv to array.
+##### Full option
+````
+$Iacsv = Iacsv::start('path file')
+	->setRowStart(2) // This is option( Default 0 )
+	->setIsUTF8(true) // This is option( Will be auto detect if you don't set )
+	->setDelimiter(';') // This is option( Will be auto detect if you don't set )
+	->all();
+	
+````
+
+##### Short way with auto detect (slow performance)
+````
+$Iacsv = Iacsv::start('path file')->all();
+````
+---
+#### #You can loop csv file and get single row content for improve performance
+````
+$Iacsv = new Iacsv($pathFile);
+$Iacsv->setRowStart(11); // This is option( Default 0 )
+$Iacsv->setIsUTF8(true); // This is option( Will be auto detect if you don't set )
+$Iacsv->setDelimiter(';'); // This is option( Will be auto detect if you don't set )
+$Iacsv->openSingleRow();
+while ($Iacsv->checkIsNotEof()) {
+	$singleRow = $Iacsv->getSingleRow(); // You can get your single row data here.
+	print_r($singleRow);
+}
+````
---
+++ src/Iacsv.php
@@ -0,0 +1,247 @@
+<?php
+
+namespace baolq2020\rdcsv;
+
+class Iacsv
+{
+
+	private $pathFile;
+
+	private $isUTF8;
+
+	private $delimiter;
+
+	private $rowStart;
+
+	private $fileContent;
+
+
+	public function __construct($pathFile)
+	{
+		$this->pathFile = $pathFile;
+	}
+
+
+	/**
+	 * Handle content file.
+	 *
+	 */
+
+	public function setFileContent($fileContent)
+	{
+		$this->fileContent = $fileContent;
+		return $this;
+	}
+
+	public function getFileContent()
+	{
+		return $this->fileContent;
+	}
+
+
+	/**
+	 * Handle Delimiter
+	 *
+	 */
+
+	public function detectDelimiter()
+	{
+
+		$delimiters = array(',' => 0, ';' => 0, "\t" => 0, '|' => 0,);
+		$firstLine = '';
+		$this->__openFile();
+		if ($this->fileContent) {
+			$firstLine = fgets($this->fileContent);
+			$this->__closeFile();
+		}
+		if ($firstLine) {
+			foreach ($delimiters as $delimiter => &$count) {
+				$count = count(str_getcsv($firstLine, $delimiter));
+			}
+			$this->delimiter = array_search(max($delimiters), $delimiters);
+		}
+		else {
+			$this->delimiter = key($delimiters);
+		}
+
+	}
+
+	public function setDelimiter($delimiter)
+	{
+		$this->delimiter = $delimiter;
+		return $this;
+
+	}
+
+	public function getDelimiter()
+	{
+		return $this->delimiter;
+	}
+
+
+	/**
+	 * Handle path file
+	 *
+	 */
+
+	public function setPathFile($pathFile)
+	{
+		$this->pathFile = $pathFile;
+		return $this;
+	}
+
+	public function getPathFile()
+	{
+		return $this->pathFile;
+	}
+
+	/**
+	 * Handle path file
+	 *
+	 */
+
+	public function setRowStart($rowStart)
+	{
+		$this->rowStart = $rowStart;
+		return $this;
+	}
+
+	public function getRowStart()
+	{
+		return $this->rowStart;
+	}
+
+
+	/**
+	 * Handle format file
+	 *
+	 */
+	public function detectFormatFileIsUTF8()
+	{
+		$this->isUTF8 = mb_check_encoding(file_get_contents($this->pathFile), 'UTF-8');
+	}
+
+	public function setIsUTF8($isUTF8)
+	{
+		$this->isUTF8 = $isUTF8;
+		return $this;
+	}
+
+	public function getFormatFileIsUTF8()
+	{
+		return $this->isUTF8;
+	}
+
+	/**
+	 * Handle file
+	 *
+	 */
+	private function __openFile()
+	{
+		$this->fileContent = fopen($this->pathFile, 'r');
+	}
+
+	private function __closeFile()
+	{
+		fclose($this->fileContent);
+	}
+
+	/**
+	 * Get data from file.
+	 *
+	 */
+
+	public static function start($pathFile)
+	{
+		return new self($pathFile);
+	}
+
+	private function __beforeGetFile()
+	{
+		if (is_null($this->getDelimiter())) {
+			$this->detectDelimiter();
+		}
+		if (is_null($this->getFormatFileIsUTF8())) {
+			$this->detectFormatFileIsUTF8();
+		}
+		if (is_null($this->getRowStart())) {
+			$this->setRowStart(0);
+		}
+	}
+
+	public function all()
+	{
+		$this->__beforeGetFile();
+		return $this->__toArray();
+	}
+
+	public function openSingleRow()
+	{
+		$this->__beforeGetFile();
+		$this->__openFile();
+		$this->__handleStartRow();
+		return $this;
+	}
+
+	public function getSingleRow()
+	{
+		return $this->__getRow();
+	}
+
+	private function __getRow()
+	{
+		if (!$this->getFormatFileIsUTF8()) {
+			$contents = str_getcsv(iconv('Windows-1252', 'UTF-8', fgets($this->fileContent)), $this->delimiter);
+		}
+		else {
+			$contents = str_getcsv(fgets($this->fileContent), $this->delimiter);
+		}
+		if (count($contents) == 1) {
+			$contents = explode(',', $contents[0]);
+		}
+		return $contents;
+	}
+
+
+	private function __toArray()
+	{
+		$return = [];
+		$this->__openFile();
+		$this->__handleStartRow();
+		while (!$this->checkIsEof()) {
+			$return[] = $this->__getRow();
+		}
+		return $return;
+	}
+
+	private function __handleStartRow()
+	{
+		if ($this->getRowStart() > 0) {
+			for ($row = 1; $row <= $this->getRowStart(); $row++) {
+				fgets($this->fileContent);
+			}
+		}
+	}
+
+
+	public function checkIsEof()
+	{
+		if (feof($this->fileContent)) {
+			$this->__closeFile();
+			return true;
+		}
+		return false;
+	}
+
+	public function checkIsNotEof()
+	{
+		if (!feof($this->fileContent)) {
+			return true;
+		}
+
+		$this->__closeFile();
+		return false;
+	}
+
+
+}
